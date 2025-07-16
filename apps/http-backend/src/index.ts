c16ff1c5c3e64309934615f0df92f4c1892c889b
Guru <vyasguru44@gmail.com>
2025-07-16 13:35:17 +0530

    added prisma and connected database with all the applications

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,29 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@repo/db/generated/prisma';
+
+const prisma = new PrismaClient();
 
 const app = express();      
+app.use(express.json());
+
+
+app.post("signup", async (req, res) => {
+    res.json({
+        message: "User signed up successfully",
+    })
+});
+
+
+app.post("/signin", async (req, res) => {
+    res.json({
+        message: "User signed in successfully",
+    })
+})
+
+
+
+
 
 app.listen(3001, () => {
   console.log('HTTP Backend is running on port 3000');
